dd0a41a0d09a08274b5896c8ea8567454fb33256
gitcold <y1658623870@163.com>
2026-03-06 23:44:27 +0800

    paddle move better

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,10 @@ const WINDOW_HEIGHT: f32 = 300.0;
 const PADDLE_WIDTH: f32 = 60.0;
 const PADDLE_HEIGHT: f32 = 10.0;
 const PADDLE_Y: f32 = 260.0;
-const PADDLE_VEC: f32 = 15.0;
+const PADDLE_VEC: f32 = 240.0;
 
 enum GameState {
     Playing,
-    GameOver,
 }
 
 #[macroquad::main("pong-rw")]
@@ -21,13 +20,17 @@ async fn main() {
     let mut game_state = GameState::Playing;
 
     loop {
-        if is_mouse_button_down(MouseButton::Left) {
-            if mouse_position().0 - paddle_x >= PADDLE_VEC {
-                paddle_x += PADDLE_VEC;
-            } else if paddle_x - mouse_position().0 >= PADDLE_VEC {
-                paddle_x -= PADDLE_VEC;
-            } else {
-                paddle_x = mouse_position().0;
+        let dt = get_frame_time();
+
+        if let GameState::Playing = game_state {
+            if is_mouse_button_down(MouseButton::Left) {
+                if mouse_position().0 - (paddle_x + PADDLE_WIDTH / 2.0) >= PADDLE_VEC * dt {
+                    paddle_x += PADDLE_VEC * dt;
+                } else if (paddle_x + PADDLE_WIDTH / 2.0) - mouse_position().0 >= PADDLE_VEC * dt {
+                    paddle_x -= PADDLE_VEC * dt;
+                } else {
+                    paddle_x = (mouse_position().0 - PADDLE_WIDTH / 2.0);
+                }
             }
         }
 
